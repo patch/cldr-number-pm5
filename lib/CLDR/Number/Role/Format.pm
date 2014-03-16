5e12f2f548323a1b40dd3af0c877a39d8d74b1fe
Nick Patch <patch@cpan.org>
2014-03-16 11:06:07 -0400

    store numbering system digits as array

--- lib/CLDR/Number/Role/Format.pm
+++ lib/CLDR/Number/Role/Format.pm
@@ -281,11 +281,11 @@ sub _format_number {
     }
 
     if ($self->numbering_system ne 'latn') {
-        my @digits = split //, $CLDR::Number::Data::System::DATA->{
+        my $digits = $CLDR::Number::Data::System::DATA->{
             $self->numbering_system
         };
 
-        $num_format =~ s{ ( [0-9] ) }{$digits[$1]}xg;
+        $num_format =~ s{ ( [0-9] ) }{$digits->[$1]}xg;
     }
 
     my $format = do { if ($negative) {
