7c8fe535cf13cf972896ce8105ebaf087655ef56
Nick Patch <patch@cpan.org>
2014-11-13 23:43:09 -0500

    templatize generated data modules using xslate [issue #33]

--- bin/generate-cldr-data.pl
+++ bin/generate-cldr-data.pl
@@ -4,6 +4,7 @@ use utf8;
 use warnings;
 use open qw( :encoding(UTF-8) :std );
 use JSON qw( decode_json );
+use Text::Xslate;
 
 my $cldr_dir = shift || '';
 
@@ -27,42 +28,22 @@ close $system_cldr_fh
 my $cldr_version = $system_cldr_data->{version}{_cldrVersion};
 my %systems      = %{$system_cldr_data->{numberingSystems}};
 
-my $system_perl_data
-    = join "\n",
-      map { sprintf '    % -8s => [qw( %s )],', $_, join ' ',
-                                                    split //,
-                                                    $systems{$_}{_digits}
-      }
-      sort
-      grep { $systems{$_}{_type} eq 'numeric' }
-      keys %systems;
+my @systems = map { { name    => sprintf('% -8s', $_),
+                      numbers => join ' ', split //, $systems{$_}{_digits} } }
+              sort
+              grep { $systems{$_}{_type} eq 'numeric' }
+              keys %systems;
 
 open my $system_pm_fh, '>', $system_pm_file
     or die "Can't open $system_pm_file: $!";
 
-printf {$system_pm_fh} <<'END_PM', $version, $cldr_version, $system_perl_data;
-package CLDR::Number::Data::System;
+my $tx = Text::Xslate->new(path => ['bin/tx']);
 
-use v5.8.1;
-use utf8;
-use strict;
-use warnings;
-
-# Supplemental Data: Numbering Systems
-
-# This module does not have a publicly supported interface and may change in
-# backward incompatible ways in the future. Please use one of the documented
-# classes instead.
-
-our $VERSION      = '%s';
-our $CLDR_VERSION = '%s';
-
-our $DATA = {
-%s
-};
-
-1;
-END_PM
+printf {$system_pm_fh} $tx->render('system.tx', {
+    version      => $version,
+    cldr_version => $cldr_version,
+    systems      => \@systems,
+});
 
 close $system_pm_fh
     or die "Can't close $system_pm_file: $!";
